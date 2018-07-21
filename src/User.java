0ef7328ac45145ad305c5207f584cc53b648f819
LeHoang <35488290+hisiter97@users.noreply.github.com>
2018-07-21 23:18:08 +0700

    Revert "Add Song.java and User.java"

--- src/User.java
+++ src/User.java
@@ -1,49 +1,9 @@
 
 public class User {
 
-	private int id;
-	private String name;
-	private String phone;
-	private String address;
-
-	public User() {}
-
-	public User(int id, String name, String phone, String address) {
-		this.id = id;
-		this.name = name;
-		this.phone = phone;
-		this.address = address;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
 	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-	public int getId() {
-		return id;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getName() {
-		return name;
-	}
- 
- 	public void setPhone(String phone) {
- 		this.phone = phone;
- 	}
- 	public String getPhone() {
- 		return phone;
- 	}
-
- 	public void setAddress(String address) {
- 		this.address = address;
- 	}
-
- 	public String getAddress() {
- 		return address;
- 	}
-
 
 }
