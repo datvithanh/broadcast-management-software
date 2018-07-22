d117b50a9bf6460eedc2a08feb34ae8cea915e2a
Le Trong Duc 20161082 <duc.lt161082@sis.hust.edu.vn>
2018-07-22 21:06:44 +0700

    up broadcast class and request class

--- src/Broadcast.java
+++ src/Broadcast.java
@@ -1,9 +1,50 @@
 
 public class Broadcast {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		private int id;
+		private String name;
+		private String broadcastAt;
+		private String createdAt;
+	
+	
+	
+	public Broadcast(int id, String name, String broadcastAt, String createdAt ) {	
+		this.id = id;
+		this.name = name;
+		this.broadcastAt = broadcastAt;
+		this.createdAt = createdAt;
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBroadcastAt() {
+		return broadcastAt;
+	}
+
+	public void setBroadcastAt(String broadcastAt) {
+		this.broadcastAt = broadcastAt;
+	}
+
+	public String getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(String createdAt) {
+		this.createdAt = createdAt;
+	}
+	
 }
--- src/Request.java
+++ src/Request.java
@@ -1,4 +1,64 @@
 
 public class Request {
+		
+	private int id;
+	private int userId;
+	private int songId;
+	private boolean resolved;
+	private String createAt;
 
+	public Request(int id, int userId, int songId, boolean resolved, String createAt) {
+		
+		this.id = id;
+		this.userId = userId;
+		this.songId = songId;
+		this.resolved = resolved;
+		this.createAt = createAt;
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getSongId() {
+		return songId;
+	}
+
+	public void setSongId(int songId) {
+		this.songId = songId;
+	}
+
+	public boolean isResolved() {
+		return resolved;
+	}
+
+	public void setResolved(boolean resolved) {
+		this.resolved = resolved;
+	}
+
+	public String getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(String createAt) {
+		this.createAt = createAt;
+	}
+	
+	
+	
 }
+
+
