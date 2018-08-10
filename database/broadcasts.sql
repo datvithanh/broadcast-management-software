e13c009521237334052deb60e7ff3b98164449eb
datvithanh98 <datvithanh98@gmail.com>
2018-08-10 15:44:00 +0700

    Minor data change

--- database/broadcasts.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO public.broadcasts (id, name, broadcast_at, created_at) VALUES (33, 'Phat song ngay 2018-08-07 lan 3', '2018-08-07 22:01:44', '2018-08-07 22:01:43.911103');
-INSERT INTO public.broadcasts (id, name, broadcast_at, created_at) VALUES (34, 'Phat song ngay 2018-08-07 lan 4', '2018-08-07 22:01:47', '2018-08-07 22:01:46.994131');
-INSERT INTO public.broadcasts (id, name, broadcast_at, created_at) VALUES (35, 'Phat song ngay 2018-08-07 lan 5', '2018-08-07 22:01:49', '2018-08-07 22:01:49.059323');
-INSERT INTO public.broadcasts (id, name, broadcast_at, created_at) VALUES (36, 'Phat song ngay 2018-08-07 lan 6', '2018-08-07 22:01:51', '2018-08-07 22:01:51.185425');
-INSERT INTO public.broadcasts (id, name, broadcast_at, created_at) VALUES (37, 'Phat song ngay 2018-08-07 lan 7', '2018-08-07 22:01:53', '2018-08-07 22:01:53.180334');
-INSERT INTO public.broadcasts (id, name, broadcast_at, created_at) VALUES (38, 'Phat song ngay 2018-08-07 lan 8', '2018-08-07 22:01:57', '2018-08-07 22:01:56.565081');
